b231c81c0c9a2b5dc96148735e9cc3b059e75e7d
Sattar Malic <Sattar Malic@DESKTOP-S13DO91>
2019-12-18 13:19:52 +0500

    change writeline method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WirteLine("new Console project");
+            Console.WriteLine("new Console project");
         }
     }
 }
